ca0617161b90bdb10079ab03a1af24a8fe5e4cf3
LEO SANTOS <leo.weverton.santos@gmail.com>
2017-07-19 16:20:46 -0300

    POC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ItemProdutoComponent } from './item-produto/item-produto.component';
+import { ListaComponent } from './lista/lista.component';
+import { CadastrarAlterarProdutoComponent } from './cadastrar-alterar-produto/cadastrar-alterar-produto.component';
+import { CadastrarAlterarListaComponent } from './cadastrar-alterar-lista/cadastrar-alterar-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent,
+    ItemProdutoComponent,
+    ListaComponent,
+    CadastrarAlterarProdutoComponent,
+    CadastrarAlterarListaComponent
   ],
   imports: [
     BrowserModule,
